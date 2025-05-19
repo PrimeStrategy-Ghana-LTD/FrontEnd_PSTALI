32623cadcd0b02d062d980df2b91255b59b4f3df
Henrrike Quarshie <henrrikequarshie@gmail.com>
2025-05-19 21:57:24 +0000

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import LoginPage from './pages/LoginPage'
 
 function App() {
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <LoginPage />
+  },
+])
 
   return (
     <>
- <h1>hi</h1>
+    <RouterProvider router={router} />
     </>
   )
 }
