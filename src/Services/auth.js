4453fcea7bcf652462455efce1fe27c227a0c7da
Faiza <faizaadulwahabfaw@gmail.com>
2025-05-22 03:50:01 -0700

    addUser conflict merge resolved

--- src/Services/auth.js
+++ src/Services/auth.js
@@ -1,5 +1,11 @@
-import { apiClient } from "./config";
+import { apiClient } from "./config"
 
 export const apiAddUser = async (payload) => {
     return await apiClient.post("/users/register", payload);
 };
+
+export const apiLogin = async (payload) =>{
+    return await apiClient.post("/users/login", payload);
+};
+
+
