07e854b9b4cd37cbee117a40ca1e25fd8d6f3f13
Faiza <faizaadulwahabfaw@gmail.com>
2025-05-26 12:12:56 -0700

    auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,11 +1,10 @@
 import { apiClient } from "./config"
 
-export const apiAddUser = async (payload) => {
-    return await apiClient.post("/users/register", payload);
+
+export const apiSignin = async(payload) => {
+    return await apiClient.post('/users/login', payload);
 };
 
-export const apiLogin = async (payload) =>{
-    return await apiClient.post("/users/login", payload);
+export const apiProfile = async() => {
+    return await apiClient.get('/user/me');
 };
-
-
