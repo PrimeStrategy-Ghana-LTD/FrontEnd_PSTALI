1b4fd96649ad8ed68fa49d727e2e83670751c8f7
Ms-Adubea <apeadutheodora@gmail.com>
2025-07-01 12:07:09 +0000

    Removed unwanted routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
 // src/App.jsx
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// Toastify import
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import SearchPage from "./pages/SearchPage";
 import AssetOverview from "./pages/AssetOverview";
 import SearchResult from "./pages/SearchResult";
 import DashboardLayout from "./Layouts/DashboardLayout";
-import Sidebar from "./components/Sidebar";
-
-// Toastify import
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Sidebar1 from "./components/Sidebar1";
-// import ViewAsset from "./pages/ViewAsset";
 import AddUser from "./pages/AddUser";
-
-// import AssignedPage from "./pages/AssignedPage";
-// import ManageStore from "./pages/ManageStore";
-import AssetCardView from "./pages/AssetCardView";
 import AddAsset from "./pages/AddAsset";
 import ManageStore from "./pages/ManageStore";
 import UserAccount from "./pages/UserAccount";
@@ -54,15 +47,6 @@ const router = createBrowserRouter([
   {
     path: "/add-asset",
     element: <AddAsset />,
-  },
-
-  {
-    path: "/overview",
-    element: <AssetOverview />,
-  },
-  {
-    path: "/card-view",
-    element: <AssetCardView />,
   },
 
   {
@@ -108,10 +92,6 @@ const router = createBrowserRouter([
       },
       { path: "manage-location", element: <ManageStore /> },
     ],
-  },
-  {
-    path: "/sidebar",
-    element: <Sidebar />,
   },
 ]);
 
